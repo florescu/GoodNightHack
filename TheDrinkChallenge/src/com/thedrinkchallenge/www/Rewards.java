c23f7f2b7dfb383a3787a9c86f4b10f5c255b895
mlrap <a.d.culea@gmail.com>
2014-11-09 07:18:00 +0000

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	TheDrinkChallenge/bin/TheDrinkChallenge.apk
    	TheDrinkChallenge/bin/classes.dex
    	TheDrinkChallenge/bin/classes/com/thedrinkchallenge/www/R$id.class
    	TheDrinkChallenge/bin/classes/com/thedrinkchallenge/www/R$integer.class
    	TheDrinkChallenge/bin/classes/com/thedrinkchallenge/www/R$layout.class
    	TheDrinkChallenge/bin/classes/com/thedrinkchallenge/www/R$string.class
    	TheDrinkChallenge/bin/classes/com/thedrinkchallenge/www/R$style.class
    	TheDrinkChallenge/bin/resources.ap_
    	TheDrinkChallenge/gen/com/thedrinkchallenge/www/R.java
    	TheDrinkChallenge/res/layout/take_test.xml
    	TheDrinkChallenge/src/com/thedrinkchallenge/www/TakeTestAction.java

---
+++ TheDrinkChallenge/src/com/thedrinkchallenge/www/Rewards.java
@@ -0,0 +1,12 @@
+package com.thedrinkchallenge.www;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Rewards extends Activity{
+	@Override
+	protected void onCreate(Bundle icicle) {
+         super.onCreate(icicle);
+         setContentView(R.layout.rewards);  
+	}
+}
